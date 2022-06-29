ef33c4487799c48c39b762c2d49097e815aace7a
0x4a5 <github@crisiscoins.com>
2022-06-29 14:50:18 -0700

    color modification for footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
     <footer className='absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-slate-600'>
-        <p>Copyright &copy; {new Date().getFullYear()} <span className="text-[#fff]">Crisis</span>Coins™</p>
+        <p>Copyright &copy; {new Date().getFullYear()} <span className="text-[red]">Crisis</span>Coins™</p>
     </footer>
   )
 }
